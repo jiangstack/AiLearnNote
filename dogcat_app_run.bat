ee8efd4a0c2ebf0e03e93c0159294e1b9c08f953
jiangstack <jiangstack@163.com>
2023-03-27 18:08:21 +0800

    猫狗大战 从vgg16上训练

--- dogcat_app_run.bat
+++ dogcat_app_run.bat
@@ -1,4 +1,5 @@
 
-CALL  D:\ProgramData\Anaconda3\Scripts\activate.bat
+%CALL  D:\ProgramData\Anaconda3\Scripts\activate.bat%
+CALL  D:\Users\jiang\anaconda3\Scripts\activate.bat ai
 python dogcat_app.py %*
 pause
